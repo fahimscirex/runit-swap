74e919142d7cf18d6d4c104fc82193f28a97d6f5
fahimscirex <fahimscirex@tutanota.com>
2022-12-03 14:36:49 +0600

    artix compatibility

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@
 cd `dirname $0`
 [ "$UID" != "0" ] && [ -f /usr/bin/sudo ] && export SUDO="sudo"
 $SUDO cp -v ./runit-swap	/usr/bin/runit-swap
-$SUDO cp -v ./swap.conf		/etc/runit/
-$SUDO cp -rv ./swap		/etc/sv/swap
+$SUDO cp -v ./swap.conf		/etc/runit-swap.conf
+$SUDO cp -rv ./swap		/etc/runit/sv/swap
